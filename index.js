5416bf2842a0b449219d303b0d75f8651aa2ff01
Denis Zavhorodnii <zdenik45@gmail.com>
2024-08-11 08:43:06 +0300

    homework

---
+++ index.js
@@ -0,0 +1,56 @@
+// let myAge = 18;
+// console.log(myAge);
+
+
+
+// const myName = "Denis";
+// console.log(myName);
+
+
+
+// let isStudent = true;
+// console.log(isStudent);
+
+
+
+// let myString = "Дисципліна - мати перемоги";
+// console.log(myString);
+
+
+
+// let myNumber = 18;
+// myNumber = myNumber + 10;
+// console.log(myNumber);
+
+
+
+// const myNull = null;
+// console.log(myNull);
+
+
+
+// const userName = prompt("Введіть своє ім'я:");
+// if (userName) {
+//   alert("Привіт, " + userName + "!");
+// } else {
+//   alert("Привіт, незнайомець!");
+// }
+
+
+
+// const confirmation = confirm("Ви впевнені?");
+// if (confirmation) {
+//     alert("Дякую за підтвердження!");
+// } else {
+//     alert("Дію відміненно!");
+// }
+
+
+
+// const alertion = alert("Ви впевнені?");
+// const confirmation = confirm("Дія небезпечна!");
+// if (confirmation) {
+//     alert("Дякую за підтвердження!");
+// } else {
+//     alert("Дію відміненно!");
+// }
