68e4410c434ebc89ea3522be4709f16dd90ba603
Denis Zavhorodnii <zdenik45@gmail.com>
2024-08-11 15:57:51 +0300

    second homework

---
+++ second homework/index.js
@@ -0,0 +1,59 @@
+// const tempCelsius = 29;
+// const tempFahrenheit = tempCelsius * 9 / 5 + 32;
+// console.log(tempFahrenheit);
+
+
+
+// const daysMonth = 31;
+// const hours = daysMonth * 24;
+// const minutes = hours * 60;
+
+// console.log(hours);
+// console.log(minutes);
+
+
+
+// let HP = 100; 
+// let stamina = 100;
+
+// HP -= 45;
+// stamina -= 61;
+
+// console.log(HP);
+// console.log(stamina);
+
+
+
+// let sum = 100; 
+// console.log(sum -= sum * 10 / 100);
+
+
+
+// let number = 76.29;
+// console.log(Math.floor(number)); 
+
+
+
+
+// const number = "1920.20;
+// console.log(Number.parseFloat(number)); 
+
+
+
+
+// const number = "1920px";
+// console.log(Number.parseInt(number));
+
+
+
+
+// const number = 5;
+// console.log(Math.sqrt(number));
+
+
+
+// const number = 12.5;
+// const text = "10rem";
+
+// console.log(typeof number.toString());
+// console.log(typeof Number.parseInt(text));
