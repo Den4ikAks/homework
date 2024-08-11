3c9772b1f39e4f534cb5749f9f99b43ab65d8edc
Denis Zavhorodnii <zdenik45@gmail.com>
2024-08-11 16:19:45 +0300

    third homework

---
+++ third homework/index.js
@@ -0,0 +1,22 @@
+// const result = 5+5+'5';
+// console.log(result);
+
+
+
+// const email = `zdenik45@gmail.com`;
+// console.log(email.includes("@"));
+// console.log(email.length);
+
+
+
+// const firstName = "My";
+// const secondName = "name";
+// const thirdName = "is";
+// const fullName = firstName + secondName + thirdName + "Victor";
+// console.log(fullName);
+
+
+
+const userName = "Денис";
+const payment = "45";
+alert(`Дякуємо, Олександро! До сплати 300 гривень`);
