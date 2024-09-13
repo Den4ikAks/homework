12717f60d023eca11881be873b57555775c5022e
Denis Zavhorodnii <zdenik45@gmail.com>
2024-09-13 14:25:23 +0300

    123

---
+++ 4/index.js
@@ -0,0 +1,56 @@
+// function checkFields() {
+//     const field1 = document.getElementById('field1').value;
+//     const field2 = document.getElementById('field2').value;
+
+    
+//     const message = document.getElementById('message');
+
+    
+//     if (field1 !== '' && field2 !== '') {
+//         message.textContent = 'Обидва поля заповнені';
+//         message.style.color = 'green';
+//     } else {
+//         message.textContent = 'Не всі поля заповнені';
+//         message.style.color = 'red';
+//     }
+// }
+
+
+
+
+
+
+// function checkSum() {
+//     const num1 = parseFloat(document.getElementById('num1').value);
+//     const num2 = parseFloat(document.getElementById('num2').value);
+
+//     const message = document.getElementById('message');
+
+    
+//     if ((num1 + num2) > 10) {
+//         message.textContent = 'Сума більша за 10';
+//         message.style.color = 'green';
+//     } else {
+//         message.textContent = 'Сума менша або дорівнює 10';
+//         message.style.color = 'red';
+//     }
+// }
+
+
+
+
+
+
+// const textInput = document.getElementById("textInput");
+// const checkButton = document.getElementById("checkButton");
+// const result = document.getElementById("result");
+
+// checkButton.addEventListener("click", function() {
+//     const text = textInput.value;
+    
+//     if (text.includes("JavaScript")) {
+//         result.textContent = "Текст містить слово JavaScript";
+//     } else {
+//         result.textContent = "Текст не містить слово JavaScript";
+//     }
+// });
